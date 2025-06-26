a945b1c5a682e10d219b546252cfae0864c12447
Julio Martins <juliomarttins5@gmail.com>
2025-06-26 15:36:51 -0300

    feat: add initial setup
    add tailwindcss, react, typescript, biome and vite

--- README.md
+++
@@ -1,2 +0,0 @@
-# monster-fight-game
-This is a Monster Fight game project, full created in the client with React + TypeScript.
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
+import { Routes } from "react-router";
+import { Route } from "react-router";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
