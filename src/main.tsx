0c77e62b3a4dcf2cca25aafd840353877520a333
Julio Martins <juliomarttins5@gmail.com>
2025-06-27 19:44:24 -0300

    chore: add default monsters

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,16 @@ import { Routes } from "react-router";
 import { Route } from "react-router";
 
 import "./index.css";
-import Menu from "./pages/Menu";
+import Menu from "./pages/menu";
+import MonsterSelection from "./pages/monster-selection";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Menu />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Menu />} />
+				<Route path="/characters" element={<MonsterSelection />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>,
 );
