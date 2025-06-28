c5f79ce260e43b0ba8cb1fb23f55cb401bc174c3
Julio Martins <juliomarttins5@gmail.com>
2025-06-28 10:57:38 -0300

    feat: change monsters images location

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,19 @@ import { Route } from "react-router";
 
 import "./index.css";
 import Menu from "./pages/menu";
-import MonsterSelection from "./pages/monster-selection";
+import MonsterSelector from "./pages/monster-selector";
+import Battle from "./pages/battle";
+import Result from "./pages/result";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<Menu />} />
-				<Route path="/characters" element={<MonsterSelection />} />
-			</Routes>
-		</BrowserRouter>
-	</StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Menu />} />
+        <Route path="/characters" element={<MonsterSelector />} />
+        <Route path="/battle" element={<Battle />} />
+        <Route path="/result/:battleId" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 );
